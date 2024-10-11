c2a00b773a3ac7e4eec31f33e62bb0795ec5166e
banibalharsh9 <banibalharsh9@gmail.com>
2024-10-11 17:49:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,20 +25,20 @@ from os import getenv
 # ---------------R---------------------------------
 API_ID = int(os.environ.get("API_ID", "23701738"))
 # ------------------------------------------------
-API_HASH = os.environ.get("API_HASH", "28f54cde54548df7354035c038ab3ddd"")
+API_HASH = os.environ.get("API_HASH")
 # ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7130342243:AAHGQ0sTu9SLiJJ58Q7XD5B0v4RF88pZfAI")
 # -----------------A-------------------------------
 BOT_USERNAME = os.environ.get("BOT_USERNAME")
 # ------------------X------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID", "1996039956"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "1376801961"))
 # ------------------X------------------------------
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1996039956").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1376801961").split()))
 # ------------------------------------------------
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002233460126"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002091954872"))
 # ------------------------------------------------
 MONGO_URL = os.environ.get("MONGO_URL")
 # -----------------------------------------------
-PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002363250260"))
+PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002297983649"))
 
