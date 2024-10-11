bda09136f51e6c7846f1827188d197b8041c02a8
banibalharsh9 <banibalharsh9@gmail.com>
2024-10-11 17:57:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ from os import getenv
 # ---------------R---------------------------------
 API_ID = int(os.environ.get("API_ID", "23701738"))
 # ------------------------------------------------
-API_HASH = os.environ.get("API_HASH")
+API_HASH = os.environ.get("API_HASH", "28f54cde54548df7354035c038ab3ddd")
 # ----------------D--------------------------------
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7130342243:AAHGQ0sTu9SLiJJ58Q7XD5B0v4RF88pZfAI")
 # -----------------A-------------------------------
@@ -38,7 +38,7 @@ SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1376801961").split()))
 # ------------------------------------------------
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002091954872"))
 # ------------------------------------------------
-MONGO_URL = os.environ.get("MONGO_URL")
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://nicenice:EwJT5zWkfBkrWXFP@cluster0.gcn92.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 # -----------------------------------------------
 PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002297983649"))
 
